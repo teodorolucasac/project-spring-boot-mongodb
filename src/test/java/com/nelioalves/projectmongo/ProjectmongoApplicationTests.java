3043e3ec829d016fee281679b41f1cc9045e5cf3
Lucas Teodoro <lucasteodoroac@gmail.com>
2022-09-05 21:12:00 -0300

    Project created

---
+++ src/test/java/com/nelioalves/projectmongo/ProjectmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.projectmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
